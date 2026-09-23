cba17afb800648461fcb79942c072a129473800d
SOULEYMANEDEV007 <cisseyacoubasouleymane4@gmail.com>
2026-09-23 14:26:52 +0000

    Fix: Change Drocolor Logo

--- lib/constants/images.ts
+++ lib/constants/images.ts
@@ -29,7 +29,7 @@ export const IMAGES = {
   // --- LOGOS DES FOURNISSEURS & PARTENAIRES AGRÉÉS ---
   partners: {
     ldf: "/images/ldf.png",
-    drocolor: "/images/drocolor-logo.jfif", // Premier logo Drocolor (fond rouge)
+    drocolor: "/images/drocolor-logo.jpg", // Premier logo Drocolor (fond rouge)
     comafrique: "/images/logo-comafrique.webp",
     inovim: "/images/logo-inovim.jpg",
     kaydan: "/images/logo-kaydan.webp",
